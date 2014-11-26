acf804c6bb0d1af9c26ed797e1cfa131db8998bf
Rafael Goes <rewgoes@gmail.com>
2014-11-26 01:25:14 -0200

    Lesson2a - Templates

--- lesson2a_templates/lesson2a.py
+++ lesson2a_templates/lesson2a.py
@@ -1,56 +1,34 @@
 import os
 
 import webapp2
+import jinja2
 
-
-form_html = """
-<form>
-<h2>Add a Food</h2>
-<input type="text" name="food" />
-%s
-<button>Add</button>
-</form>
-"""
-
-hidden_html = """
-<input type="hidden" name="food" value="%s" />
-"""
-
-item_html = "<li>%s</li>"
-
-
-shopping_list_html = """
-<br />
-<br />
-<h2>Shopping List</h2>
-<ul>
-%s
-</ul>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape=True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
 class MainPage(Handler):
     def get(self):
-        output = form_html
-        output_hidden = ""
-
         items = self.request.get_all("food")
+        self.render("shopping_list.html", items = items)
 
-        if items:
-            output_items = ""
-            for item in items:
-                output_hidden += hidden_html % item
-                output_items += item_html % item
+class FizzBuzzHandler(Handler):
+    def get(self):
+        n = self.request.get('n', 0)
+        n = n and int(n)
+        self.render('fizzbuzz.html', n = n)
 
-            output_shopping = shopping_list_html % output_items
-            output += output_shopping
+routes = [('/', MainPage),
+          ('/fizzbuzz', FizzBuzzHandler),]
 
-        output = output % output_hidden
-
-        self.write(output)
-
-
-app = webapp2.WSGIApplication([('/', MainPage),],debug=True)
+app = webapp2.WSGIApplication(routes,debug=True)
